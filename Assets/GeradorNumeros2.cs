5656f0950740a3ac760ba8d74b6d5a06c3c32572
Guilhermeaug <Guilhermeaug@users.noreply.github.com>
2019-09-15 18:29:05 -0300

    Primeira versao simultanea, varias cenas?

---
+++ Assets/GeradorNumeros2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeradorNumeros2 : MonoBehaviour
+{
+
+    public InputField numero;
+    public GameObject prefabButton;
+    public RectTransform ParentPanel1, ParentPanel2;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void gerarNumeros()
+    {
+        string valor = numero.text;
+
+        MyAwesomeCreator(valor);
+
+    }
+
+    void MyAwesomeCreator(string valor)
+    {
+        GameObject goButton1 = (GameObject)Instantiate(prefabButton);
+        GameObject goButton2 = (GameObject)Instantiate(prefabButton);
+
+        Text valorBtn1 = goButton1.GetComponentInChildren<Text>();
+        valorBtn1.text = valor;
+        Text valorBtn2 = goButton2.GetComponentInChildren<Text>();
+        valorBtn2.text = valor;
+
+        goButton1.transform.SetParent(ParentPanel1, false);
+        goButton2.transform.SetParent(ParentPanel2, false);
+    }
+
+}
